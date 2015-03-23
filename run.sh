ad3e7df4281fc64665a8853ed344c5f4418ea59a
Hemanth Kiran Dharmagadda Swamy <DSHEMANTH@users.noreply.github.com>
2015-03-23 01:54:24 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@
 # first I'll load all my dependencies
 
 # next I'll make sure that all my programs (written in Python in this example) have the proper permissions
-chmod a+x word_count.py
+chmod a+x ./src/word_count.py
 chmod a+x running_median.py
 
 # finally I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
-python ./src/word_count.py ./wc_input ./wc_output/wc_result.txt
-python ./src/running_median.py ./wc_input ./wc_output/med_result.txt
+python ./src/word_count.py ./wc_input ./wc_output
+python ./src/running_median.py ./wc_input ./wc_output
